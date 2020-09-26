05b5e8e26afeebd128b1b3411a61009082a3113e
kjwook7522 <kjwook7522@gmail.com>
2020-09-26 21:50:11 +0900

    Login, Rel 페이지 기본 구성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Login, Rel } from "pages";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      This is app
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/rel" component={Rel} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Rel/Rel.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Rel.css";
+
+function Rel() {
+  return <div>release</div>;
+}
+
+export default Rel;
