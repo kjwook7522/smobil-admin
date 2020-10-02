a49c98e8b2bcb15ce5f892b88bd80bd2d09d3f5d
kjwook7522 <kjwook7522@gmail.com>
2020-10-02 23:21:33 +0900

    버튼 디자인 수정

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
 import "./Login.css";
 
 function Login() {
   const handleAuthClick = () => {
     window.gapi.auth2.getAuthInstance().signIn();
-  }
+  };
   return (
     <section id="login">
       <div className="login-frame">
         <img src="/logo_bl.png" alt="logo" />
-        <button onClick={handleAuthClick}>Google로 로그인</button>
+        <button onClick={handleAuthClick}>
+          <FcGoogle />
+          <span>구글로 로그인</span>
+        </button>
       </div>
     </section>
   );
--- src/pages/Rel/components/Header/Header.jsx
+++ src/pages/Rel/components/Header/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
     <section id="header">
       <img src="/logo_wh.png" alt="logo" />
       <h1 className="name">{name} 기사님</h1>
-      <button onClick={handleSignoutClick}>로그아웃</button>
+      <button className="logout-btn" onClick={handleSignoutClick}>로그아웃</button>
     </section>
   );
 }
