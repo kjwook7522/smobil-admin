61c805af24056fca9062c6d6eb0d220a69ef8247
kjwook7522 <kjwook7522@gmail.com>
2020-10-03 00:37:16 +0900

    google sheet 연동

--- src/pages/Rel/components/Cart/Cart.jsx
+++ src/pages/Rel/components/Cart/Cart.jsx
@@ -1,13 +1,91 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import "./Cart.css";
 
 function Cart() {
-  const temp = [
-    { type: "배터리", name: "KV1", counts: 5 },
-    { type: "배터리", name: "KV2", counts: 1 },
-    { type: "와이퍼", name: "AVG-G1", counts: 2 },
-    { type: "오일", name: "SS01", counts: 20 },
-  ];
+  const sheetname = "driver1";
+  const [myCart, setMyCart] = useState([]);
+
+  useEffect(() => {
+    window.gapi.client.sheets.spreadsheets.values
+      .get({
+        spreadsheetId: "1UvqnHHLpQIZHUNEERvyJ-2YGhYhBDPYxHbul3Jm9qp0",
+        range: `${sheetname}!A2:D`,
+      })
+      .then(
+        response => {
+          setMyCart(response.result.values);
+        },
+        reason => {
+          console.log(reason.result.error.message);
+        }
+      );
+  }, []);
+
+  const keepProd = e => {
+    const prodId = e.target.parentElement.parentElement.id;
+    const prodIdx = myCart.findIndex(item => item[0] === prodId);
+    const prodCount = Number(myCart[prodIdx][3]);
+
+    const value = {
+      values: [[prodCount + 1]],
+    };
+
+    // google sheet update
+    window.gapi.client.sheets.spreadsheets.values
+      .update({
+        spreadsheetId: "1UvqnHHLpQIZHUNEERvyJ-2YGhYhBDPYxHbul3Jm9qp0",
+        range: `${sheetname}!D${prodIdx + 2}`,
+        valueInputOption: "RAW",
+        resource: value,
+      })
+      .then(
+        response => {
+          console.log(`${response.result.updatedCells} cell updated`);
+        },
+        reason => {
+          console.log(reason.result.error.message);
+        }
+      );
+
+    // local update
+    const updCart = myCart.slice();
+    updCart[prodIdx][3] = prodCount + 1;
+    setMyCart(updCart);
+  };
+
+  const sellProd = e => {
+    const prodId = e.target.parentElement.parentElement.id;
+    const prodIdx = myCart.findIndex(item => item[0] === prodId);
+    const prodCount = Number(myCart[prodIdx][3]);
+
+    const value = {
+      values: [[prodCount - 1]],
+    };
+
+    // google sheet update
+    window.gapi.client.sheets.spreadsheets.values
+      .update({
+        spreadsheetId: "1UvqnHHLpQIZHUNEERvyJ-2YGhYhBDPYxHbul3Jm9qp0",
+        range: `${sheetname}!D${prodIdx + 2}`,
+        valueInputOption: "RAW",
+        resource: value,
+      })
+      .then(
+        response => {
+          console.log(`${response.result.updatedCells} cell updated`);
+        },
+        reason => {
+          console.log(reason.result.error.message);
+        }
+      );
+
+    // local update
+    const updCart = myCart.slice();
+    updCart[prodIdx][3] = prodCount - 1;
+    setMyCart(updCart);
+  };
+
   return (
     <section id="cart">
       <h1>내 트렁크</h1>
@@ -21,14 +99,18 @@ function Cart() {
           </tr>
         </thead>
         <tbody>
-          {temp.map(item => (
-            <tr>
-              <td className="type">{item.type}</td>
-              <td className="name">{item.name}</td>
-              <td className="counts">{item.counts}</td>
+          {myCart.map(item => (
+            <tr key={item[0]} id={item[0]}>
+              <td className="type">{item[1]}</td>
+              <td className="name">{item[2]}</td>
+              <td className="counts">{item[3]}</td>
               <td className="btn">
-                <button className="keep-btn">재고</button>
-                <button className="sell-btn">판매</button>
+                <button className="keep-btn" onClick={keepProd}>
+                  재고
+                </button>
+                <button className="sell-btn" onClick={sellProd}>
+                  판매
+                </button>
               </td>
             </tr>
           ))}
