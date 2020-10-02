6724d6d30d0248bf518f7a6002572ff2c8cd3691
kjwook7522 <kjwook7522@gmail.com>
2020-10-02 23:28:56 +0900

    로딩 이미지 및 애니메이션 추가

--- src/common/Loading/Loading.jsx
+++ src/common/Loading/Loading.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { FaSpinner } from "react-icons/fa";
 import "./Loading.css";
 
 function Loading() {
-  return <div id="test">loading</div>;
+  return (
+    <div id="loading">
+      <FaSpinner className="fa=-spain" />
+    </div>
+  );
 }
 
 export default Loading;
