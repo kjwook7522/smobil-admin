81d7549aceabe54d7d923be71a1670d73e7bc214
kjwook7522 <kjwook7522@gmail.com>
2020-10-02 22:04:23 +0900

    카테고리 부분 추가

--- src/pages/Rel/components/Category/Category.jsx
+++ src/pages/Rel/components/Category/Category.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import { FaBatteryHalf } from "react-icons/fa";
+import { RiOilLine } from "react-icons/ri";
+import { AiOutlineTable } from "react-icons/ai";
+import { BiTachometer } from "react-icons/bi";
 import "./Category.css";
 
 function Category() {
-  return <div>Category</div>;
+  return (
+    <section id="category">
+      <div className="category-wrapper">
+        <div className="item">
+          <FaBatteryHalf /><br />
+          <p>배터리</p>
+        </div>
+        <div className="item">
+          <RiOilLine />
+          <p>오일</p>
+        </div>
+        <div className="item">
+          <AiOutlineTable />
+          <p>필터</p>
+        </div>
+        <div className="item">
+          <BiTachometer />
+          <p>와이퍼</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Category;
