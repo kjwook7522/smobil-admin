272f386c2ba7d0ecd02f8c59751e83aa78f63766
kjwook7522 <kjwook7522@gmail.com>
2020-12-29 01:35:12 +0900

    admin sales 페이지 디스플레이 구현

--- src/pages/Admin/components/SalesLog/SalesLog.jsx
+++ src/pages/Admin/components/SalesLog/SalesLog.jsx
@@ -1,19 +1,66 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { TiArrowBack } from 'react-icons/ti';
-import { categoryStruct } from 'common';
+import { categoryStruct, spreadsheetId } from 'common';
 import './SalesLog.css';
 
 function SalesLog({ setCategory }) {
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    const sheetname = 'log';
+
+    window.gapi.client.sheets.spreadsheets.values
+      .get({
+        spreadsheetId,
+        range: `${sheetname}!A2:G`,
+      })
+      .then(
+        response => {
+          setLogs(response.result.values);
+        },
+        reason => {
+          console.log(reason.result.error.message);
+        }
+      );
+  }, []);
   return (
     <section id="sales-log">
       <div className="header">
-        <h1>창고 재고 확인</h1>
+        <h1>판매 기록</h1>
         <TiArrowBack
           onClick={() => {
             setCategory({ ...categoryStruct, menu: true });
           }}
         />
       </div>
+
+      <table className="sales-table">
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>품목</th>
+            <th>상세명</th>
+            <th>수량</th>
+            <th>담당자</th>
+            <th>상태</th>
+            <th>시간</th>
+          </tr>
+        </thead>
+        <tbody>
+          {logs.map((item, idx) => (
+            <tr key={idx}>
+              <td>{item[0]}</td>
+              <td>{item[1]}</td>
+              <td>{item[2]}</td>
+              <td>{item[3]}</td>
+              <td>{item[4]}</td>
+              <td>{item[5]}</td>
+              <td>{item[6]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </section>
   );
 }
