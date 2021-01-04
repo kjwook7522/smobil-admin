b98abfb39bf9a22948215594fe8df7228a37f9d3
kjwook7522 <kjwook7522@gmail.com>
2021-01-05 00:03:00 +0900

    spread sheet id 변경

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,4 +1,4 @@
-export const spreadsheetId = '1UvqnHHLpQIZHUNEERvyJ-2YGhYhBDPYxHbul3Jm9qp0';
+export const spreadsheetId = '1BFlXKbd0mMxwT6q5wLSZU4rFdEtU_GgxM_Q_vVJu3EA';
 export const categoryStruct = {
   drivers: false,
   storage: false,
