c17d5a6bf9d159a9752e47db45d79f4cd9c1cbc5
kjwook7522 <kjwook7522@gmail.com>
2021-01-10 02:35:46 +0900

    창고 재고 확인 카테고리별 정렬

--- src/pages/Admin/components/Storage/Storage.jsx
+++ src/pages/Admin/components/Storage/Storage.jsx
@@ -5,7 +5,20 @@ import { categoryStruct, getSheetValues, updateSheetSingleValue } from 'common';
 import './Storage.css';
 
 function Storage({ setCategory }) {
+  const initialCategory = {
+    airFilter: [],
+    oilFilter: [],
+    airconFilter: [],
+    battery: [],
+    wiper: [],
+    oil: [],
+    washer: [],
+    pad: [],
+    etc: [],
+  };
+
   const [storageList, setStorageList] = useState([]);
+  const [categoryList, setCategoryList] = useState(initialCategory);
   const [inputs, setInputs] = useState({});
 
   const handleInput = e => {
@@ -36,8 +49,118 @@ function Storage({ setCategory }) {
     );
   };
 
+  const renderTables = () => {
+    let tablesJsx = [];
+    for (let category in categoryList) {
+      tablesJsx.push(
+        <React.Fragment key={category}>
+          <h1 className="stock-table-header">{chgNmToK(category)}</h1>
+          <table className="storage-stock-table">
+            <thead>
+              <tr>
+                <th width="15%">품목</th>
+                <th>상세명</th>
+                <th width="15%">수량</th>
+                <th width="15%">관리</th>
+              </tr>
+            </thead>
+            <tbody>
+              {categoryList[category].map(item => (
+                <tr key={item[0]} id={item[0]}>
+                  <td>{item[1]}</td>
+                  <td>{item[2]}</td>
+                  <td>
+                    <input type="number" value={getMyInput(item[0])} onChange={handleInput} />
+                  </td>
+                  <td>
+                    <button onClick={applySheet}>적용</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </React.Fragment>
+      );
+    }
+    return tablesJsx;
+  };
+
+  const chgNmToK = category => {
+    switch (category) {
+      case "battery":
+        return "배터리";
+      case "oil":
+        return "오일";
+      case "oilFilter":
+        return "오일필터";
+      case "airFilter":
+        return "에어필터";
+      case "wiper":
+        return "와이퍼";
+      case "washer":
+        return "워셔액";
+      case "pad":
+        return "패드";
+      case "airconFilter":
+        return "에어컨필터";
+      case "etc":
+        return "기타";
+      default:
+        return;
+    }
+  };
+
   useEffect(() => {
     const sheetname = 'storage';
+
+    const splitCategory = list => {
+      let tempCategoryList = {
+        airFilter: [],
+        oilFilter: [],
+        airconFilter: [],
+        battery: [],
+        wiper: [],
+        oil: [],
+        washer: [],
+        pad: [],
+        etc: [],
+      };
+
+      list.forEach(item => {
+        switch (item[1]) {
+          case '에어필터':
+            tempCategoryList.airFilter.push(item);
+            break;
+          case '오일필터':
+            tempCategoryList.oilFilter.push(item);
+            break;
+          case '에어컨필터':
+            tempCategoryList.airconFilter.push(item);
+            break;
+          case '배터리':
+            tempCategoryList.battery.push(item);
+            break;
+          case '와이퍼':
+            tempCategoryList.wiper.push(item);
+            break;
+          case '오일':
+            tempCategoryList.oil.push(item);
+            break;
+          case '워셔액':
+            tempCategoryList.washer.push(item);
+            break;
+          case '패드':
+            tempCategoryList.pad.push(item);
+            break;
+          case '기타':
+            tempCategoryList.etc.push(item);
+            break;
+          default:
+            break;
+        }
+      });
+      setCategoryList(tempCategoryList);
+    };
 
     getSheetValues(`${sheetname}!A2:D`).then(
       response => {
@@ -50,6 +173,7 @@ function Storage({ setCategory }) {
 
         setStorageList(data);
         setInputs(tempInputs);
+        splitCategory(data);
       },
       reason => {
         console.log(reason.result.error.message);
@@ -68,6 +192,8 @@ function Storage({ setCategory }) {
         />
       </div>
 
+      {renderTables()}
+      {/* <h1 className="stock-table-header">123</h1>
       <table className="storage-stock-table">
         <thead>
           <tr>
@@ -91,7 +217,7 @@ function Storage({ setCategory }) {
             </tr>
           ))}
         </tbody>
-      </table>
+      </table> */}
     </section>
   );
 }
